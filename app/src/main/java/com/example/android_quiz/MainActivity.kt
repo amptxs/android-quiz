3c998715f9fbc123035793c4342de9c78353a385
amptxs <72046996+amptxs@users.noreply.github.com>
2021-11-15 23:38:04 +0500

    Quiz design

--- app/src/main/java/com/example/android_quiz/MainActivity.kt
+++ app/src/main/java/com/example/android_quiz/MainActivity.kt
@@ -2,9 +2,8 @@ package com.example.android_quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import androidx.core.view.isVisible
 import com.example.android_quiz.controllers.QuizController
+import com.example.android_quiz.models.Question
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,19 +13,21 @@ class MainActivity : AppCompatActivity() {
     }
     companion object{
         var instance: MainActivity? = null
+        var QuestionsList: MutableList<Question> = mutableListOf()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         instance = this
-
         bottomNavView.selectedItemId = R.id.nav_quiz
-        quizController.bind()
 
-    }
-
-    fun getInstance(): MainActivity? {
-        return instance
+        var test1 = Question()
+        var test2 = Question()
+        test1.Label = "В каком городе лисицы живут на окраинах и иногда появляются в центре?"
+        test2.Label = "test2"
+        QuestionsList.add(test1)
+        QuestionsList.add(test2)
+        quizController.bind(QuestionsList)
     }
 }
--- app/src/main/java/com/example/android_quiz/controllers/QuizController.kt
+++ app/src/main/java/com/example/android_quiz/controllers/QuizController.kt
@@ -1,107 +1,150 @@
 package com.example.android_quiz.controllers
 
-import android.app.Activity
+import android.content.res.ColorStateList
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isGone
 import androidx.core.view.isVisible
+import androidx.core.view.updateLayoutParams
 import com.example.android_quiz.MainActivity
 import com.example.android_quiz.R
 import com.example.android_quiz.models.Question
 import kotlinx.android.synthetic.main.activity_main.*
+import android.text.SpannableString
+import android.text.Spannable
+import android.text.style.AbsoluteSizeSpan
 
-class QuizController() {
-    private val ParentActivity = MainActivity.instance!!
-    private val ArrayOfAnswerButtons = arrayOf(ParentActivity.quiz_var_a, ParentActivity.quiz_var_b,
-        ParentActivity.quiz_var_c,ParentActivity.quiz_var_d)
-
-    private var Stage: Int = 0
-    private var QuestionsList: MutableList<Question> = mutableListOf()
-    private var CorrectAnswers: Int = 0
-    private var CurrentQuestion: Question? = null
-    private var CurrentQuestionNumber: Int = 0
+class QuizController {
+    private val _parentActivity = MainActivity.instance!!
+    private val _arrayOfAnswerButtons = arrayOf(_parentActivity.quiz_var_a, _parentActivity.quiz_var_b,
+        _parentActivity.quiz_var_c,_parentActivity.quiz_var_d)
+    private var _questionsList: MutableList<Question> = mutableListOf()
+    private var _correctAnswers: Int = 0
+    private var _currentQuestion: Question? = null
+    private var _currentQuestionNumber: Int = 0
 
 
-    fun bind() {
+    fun bind(questionsList: MutableList<Question>) {
+        _questionsList = questionsList
         firstStage()
-
-        var test1 = Question()
-        var test2 = Question()
-        test1.Label = "test1"
-        test2.Label = "test2"
-        QuestionsList.add(test1)
-        QuestionsList.add(test2)
-
-        ParentActivity.quizActionButton.setOnClickListener {
+        _parentActivity.quizActionButton.setOnClickListener {
                 secondStage()
         }
     }
 
-    fun firstStage(){
-        ParentActivity.quiz_answers.isVisible = false
+    private fun firstStage(){
+        _parentActivity.quiz_answers.isVisible = false
+        _parentActivity.quizProgressText.isGone = true
     }
 
-    fun secondStage(){
-        ParentActivity.quiz_answers.isVisible = true
-        buttonInactiveCondition()
-        ParentActivity.quizActionButton.text = ParentActivity.getString(R.string.proceed)
+    private fun secondStage(){
+        _parentActivity.quiz_answers.isVisible = true
+        _parentActivity.quizProgressText.isGone = false
 
-        if (CurrentQuestionNumber != QuestionsList.size)
-            constructQuestionDisplay(QuestionsList[CurrentQuestionNumber])
+        buttonInactiveCondition()
+        _parentActivity.quizActionButton.text = _parentActivity.getString(R.string.proceed)
+
+        if (_currentQuestionNumber != _questionsList.size)
+            constructQuestionDisplay(_questionsList[_currentQuestionNumber])
         else
             thirdStage()
     }
 
-    fun thirdStage(){
-        ParentActivity.quizMainText.text = CorrectAnswers.toString()+"/"+QuestionsList.size
-        ParentActivity.quiz_answers.isVisible = false
-        ParentActivity.quizActionButton.isVisible = false
+    private fun thirdStage(){
+        _parentActivity.quiz_answers.isGone = true
+        _parentActivity.quizActionButton.isVisible = false
+
+        _parentActivity.quizProgressText.background = null
+        _parentActivity.quizProgressText.setTextColor(_parentActivity.getColor(R.color.white))
+
+        val span: Spannable = SpannableString("\n🎉 \nПоздравляем!")
+        span.setSpan(AbsoluteSizeSpan(160, true), 1, 3, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        span.setSpan(AbsoluteSizeSpan(50, true), 5, 17, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        _parentActivity.quizProgressText.text = span
+        _parentActivity.quizProgressText.gravity = Gravity.CENTER
+
+        var declension = "вопрос"
+        if (_correctAnswers == 0 || _correctAnswers > 4)
+            declension = "вопросов"
+        else if (_correctAnswers in 2..4)
+            declension = "вопроса"
+
+        _parentActivity.quizMainText.text=
+            "Вы ответили на " + _correctAnswers.toString()+" "+declension+" из "+_questionsList.size + ".\n" +
+                    "Спасибо за проявленный интерес к нашей викторине!"
+        _parentActivity.quizMainText.gravity = Gravity.CENTER
+
     }
 
-    fun constructQuestionDisplay(question: Question){
-        CurrentQuestion = question
-        ParentActivity.quizMainText.text = question.Label
+    private fun constructQuestionDisplay(question: Question){
+        _currentQuestion = question
+        val number = _questionsList.indexOf(question) + 1
+        _parentActivity.quizProgressText.text =
+            _parentActivity.getString(R.string.question) + " "+ number + "/" + _questionsList.size
 
-        for (i in ArrayOfAnswerButtons.indices) {
-            ArrayOfAnswerButtons[i].text = question.VarArray[i]
-            ArrayOfAnswerButtons[i].setOnClickListener {
+        _parentActivity.quizMainText.text = question.Label
 
-                answerCheck(ArrayOfAnswerButtons[i])
+        for (i in _arrayOfAnswerButtons.indices) {
+            _arrayOfAnswerButtons[i].text = question.VarArray[i]
+            _arrayOfAnswerButtons[i].setOnClickListener {
+                answerCheck(_arrayOfAnswerButtons[i])
             }
         }
     }
 
-    fun answerCheck(view: View){
+    private fun answerCheck(view: View){
+        _parentActivity.correctAnnotation.updateLayoutParams<ConstraintLayout.LayoutParams> {
+            endToEnd = view.id
+            topToTop = view.id
+            marginEnd = 6
+        }
 
-        if (CurrentQuestion!!.VarArray.indexOf((view as Button).text) == CurrentQuestion!!.Correct)
-            CorrectAnswers++
+        if (_currentQuestion!!.VarArray.indexOf((view as Button).text) == _currentQuestion!!.Correct)
+        {
+            correctAnswer()
+        }
+        else
+            incorrectAnswer()
 
-        CurrentQuestionNumber++
+        _currentQuestionNumber++
         buttonActiveCondition()
-
     }
 
-    fun buttonActiveCondition(){
-        ParentActivity.quizActionButton.isEnabled = true
-        ParentActivity.quizActionButton.setTextColor(ParentActivity.getColor(R.color.black))
+    private fun buttonActiveCondition(){
+        _parentActivity.quizActionButton.isEnabled = true
+        _parentActivity.quizActionButton.setTextColor(_parentActivity.getColor(R.color.black))
         answersButtonInactiveCondition()
     }
 
-    fun buttonInactiveCondition(){
-        ParentActivity.quizActionButton.isEnabled = false
-        ParentActivity.quizActionButton.setTextColor(ParentActivity.getColor(R.color.element_inactive))
+    private fun buttonInactiveCondition(){
+        _parentActivity.quizActionButton.isEnabled = false
+        _parentActivity.quizActionButton.setTextColor(_parentActivity.getColor(R.color.element_inactive))
         answersButtonActiveCondition()
     }
 
-    fun answersButtonActiveCondition()
+    private fun answersButtonActiveCondition()
     {
-        for (button in ArrayOfAnswerButtons)
-            button.isEnabled = true;
+        for (button in _arrayOfAnswerButtons)
+            button.isEnabled = true
+
+        _parentActivity.correctAnnotation.isVisible = false
     }
-    fun answersButtonInactiveCondition()
+    private fun answersButtonInactiveCondition()
     {
-        for (button in ArrayOfAnswerButtons)
-            button.isEnabled = false;
+        for (button in _arrayOfAnswerButtons)
+            button.isEnabled = false
+
+        _parentActivity.correctAnnotation.isVisible = true
     }
 
+    private fun correctAnswer(){
+        _correctAnswers++
+        _parentActivity.correctAnnotation.backgroundTintList = ColorStateList.valueOf(_parentActivity.getColor(R.color.correct))
+    }
+
+    private fun incorrectAnswer(){
+        _parentActivity.correctAnnotation.backgroundTintList = ColorStateList.valueOf(_parentActivity.getColor(R.color.incorrect))
+    }
 }
